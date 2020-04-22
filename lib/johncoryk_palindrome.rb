c383f0450b686bda5ddb8c1fdebb87c7552b6bc2
Cory Kelley <coryk0789@gmail.com>
2020-04-22 18:42:50 -0400

    Initialize repo

---
+++ lib/johncoryk_palindrome.rb
@@ -0,0 +1,6 @@
+require "johncoryk_palindrome/version"
+
+module JohncorykPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/johncoryk_palindrome/version.rb
@@ -0,0 +1,3 @@
+module JohncorykPalindrome
+  VERSION = "0.1.0"
+end
